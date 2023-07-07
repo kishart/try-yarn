cac265a4940478d4e8544adb605a14a6c68f6f81
peach <amoralyse21@gmail.com>
2023-07-08 00:38:46 +0800

    fix ni gurang

--- src/components/topbar/Topbar.jsx
+++ src/components/topbar/Topbar.jsx
@@ -1,16 +1,17 @@
 import "./topbar.css";
-import { Search, Person } from "@material-ui/icons";
+import { Search, Person } from "@mui/icons-material";
+
 export default function TopBar() {
   return (
     <div className="topbarContainer">
-      <div className="topbarLefT">
-        <span className="logo">LamaSocial</span>
+      <div className="topbarLeft">
+        <span className="logo">Lamasocial</span>
       </div>
       <div className="topbarCenter">
         <div className="searchbar">
           <Search />
           <input
-            placeholder="Search for friends, post or videos"
+            placeholder="Search for friend, post or video"
             className="searchInput"
           />
         </div>
@@ -23,6 +24,7 @@ export default function TopBar() {
         <div className="topbarIcons">
           <div className="topbarIconItem">
             <Person />
+            <span className="topbarIconBadge"></span>
           </div>
         </div>
       </div>
